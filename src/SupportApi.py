bff8352f6f0a6c6ce398fd4d1df5e2555c4eb19f
apelsito <mrappletastico@gmail.com>
2024-11-02 19:48:53 +0100

    Luz

--- src/SupportApi.py
+++ src/SupportApi.py
@@ -1,12 +1,27 @@
 # Se importa la librería para la llamada a la API
 import requests
 import pandas as pd
+import json
+
 
 
 def hacer_request_json(url):
-    result = requests.get(url)
-    json = result.json()
-    return json
+    try:
+        result = requests.get(url)
+        result.raise_for_status() 
+        json_data = result.json()  
+        return json_data
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Error HTTP: {http_err}")  # Error específico de HTTP
+    except requests.exceptions.ConnectionError:
+        print("Error de conexión. Verifica la URL o tu conexión a internet.")
+    except requests.exceptions.Timeout:
+        print("La solicitud ha tardado demasiado. Intenta de nuevo más tarde.")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Error inesperado en la solicitud: {req_err}")
+    except ValueError:
+        print("La respuesta no contiene un JSON válido.")
+    return None
 
 
 def api_gas(fecha_inicio,fecha_final):
@@ -28,9 +43,37 @@ def api_gas(fecha_inicio,fecha_final):
     df_filtrado = df_gas[cond1 & cond2]
     return df_filtrado
 
-def api_luz(years):
-    
+def api_luz(years,ruta_guardado):
+    df = pd.DataFrame()
     for year in years:
+        url = f"https://apidatos.ree.es/es/datos/mercados/componentes-precio?start_date={year}-01-01T00:00&end_date={year}-12-31T23:59&time_trunc=month&geo_limit=ccaa"
+        print(f"Realizando Request a: {url}")
+        jason = hacer_request_json(url)
         
-    
-    return
+        if jason is not None:
+            # Guardar JSON en archivo
+            with open(f"../datos/02_PreciosLuz/01_JsonBackups/PreciosLuz{year}.json", "w") as file:
+                json.dump(jason, file, indent=4)  # Guarda el JSON en un archivo
+            
+            print(f"Backup Realizado: PreciosLuz{year}.json")
+            
+            precios = jason["included"][-1]["attributes"]["values"]
+            fechas = []
+            costes = []
+            for precio in precios:
+                costes.append(precio["value"])
+                fechas.append(precio["datetime"])
+
+            df_temp = pd.DataFrame({
+                "precio": costes,
+                "fecha": fechas
+            })
+            df = pd.concat([df, df_temp], ignore_index=True)
+        else:
+            print(f"No se pudo obtener datos para el año {year}")
+   
+    df.index = df.index + 1
+    df.reset_index(inplace=True)
+    df.to_csv(f"{ruta_guardado}/01_PreciosLuzSinLimpiar.csv")
+    print(f"Dataframe guardado en: {ruta_guardado}")
+    return df
